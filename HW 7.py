1dc95bdeac040152de4295af01d80159755f0c54
topoliant <Awesomeidea19>
2021-02-08 21:27:11 +0200

    Homework 7

--- HW 7.py
+++ HW 7.py
@@ -20,17 +20,22 @@ print(my_print("I am the string"))
 ############################
 # 4
 
-persons = [{"name": "John", "age": 15}, {"name": "Arthur", "age": 55}, {"name": "Carolyne", "age": 21}, {"name": "Vasya", "age": 34}, {"name": "Harry", "age": 98}]
+persons = [{"name": "John", "age": 15},
+           {"name": "Arthur", "age": 55},
+           {"name": "Carolyne", "age": 21},
+           {"name": "Vasya", "age": 34},
+           {"name": "Harry", "age": 98}]
+print("==============")
 
 # a
 age_list = []
 name_list = []
 for item in persons:
-    for name, age in {item.values()}:
+    for name, age in [item.values()]:
         age_list.append(age)
         name_list.append(name)
     if min(age_list) in item.values():
-        print(name)
+        print(name, age)
 # b
 for name in name_list:
     maximal = max(len(name) for name in name_list)
@@ -44,7 +49,7 @@ print(average)
 ############################
 # 5
 my_dict_1 = {"a": 12, "j": 43, "q": 56, "u": "er", 1: 15}
-my_dict_2 = {"q": 11, "j": "temp", 13: "cat", 15: 5, "b": 100}
+my_dict_2 = {"q": 11, "j": "temp", 13: "cat", 15: 5, "b": 100, "u": "three"}
 
 # a
 common_dict_set = set(my_dict_1.keys()).intersection(set(my_dict_2.keys()))
@@ -61,12 +66,13 @@ for key, value in my_dict_1.items():
     if key in unique_key_1:
         key_value_1.update({key: value})
 print(key_value_1)
+
 # d
 updated_dict = {}
-for key, value in my_dict_1.items():
-    for key1, value1 in my_dict_2.items():
-        if key == key1:
-            my_dict_1[key] = [value, value1]
-updated_dict.update(my_dict_2)
 updated_dict.update(my_dict_1)
+updated_dict.update(my_dict_2)
+for key, value in my_dict_1.items():
+     for key1, value1 in my_dict_2.items():
+            if key == key1:
+                updated_dict[key] = [value, value1]
 print(updated_dict)
