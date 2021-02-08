d3627fc3b49252c28cafd5bce08012d0b8dd1396
topoliant <Awesomeidea19>
2021-02-08 22:52:02 +0200

    Homework 7

--- HW 7.py
+++ HW 7.py
@@ -1,8 +1,8 @@
 from random import randint
 
 # 1
-rndm20 = [randint(1, 100) for index in range(20)]
-print(rndm20)
+random_20_numbers = [randint(1, 100) for index in range(20)]
+print(random_20_numbers)
 
 ###########################
 # 2
@@ -20,12 +20,11 @@ print(my_print("I am the string"))
 ############################
 # 4
 
-persons = [{"name": "John", "age": 15},
-           {"name": "Arthur", "age": 55},
+persons = [{"name": "John", "age": 17},
+           {"name": "Arthur", "age": 10},
            {"name": "Carolyne", "age": 21},
-           {"name": "Vasya", "age": 34},
+           {"name": "Vasya", "age": 9},
            {"name": "Harry", "age": 98}]
-print("==============")
 
 # a
 age_list = []
@@ -34,15 +33,17 @@ for item in persons:
     for name, age in [item.values()]:
         age_list.append(age)
         name_list.append(name)
-    if min(age_list) in item.values():
-        print(name, age)
-# b
+for item in persons:
+    if min(set(age_list)) is item["age"]:
+        print(item["name"])
+
+# б
 for name in name_list:
-    maximal = max(len(name) for name in name_list)
+    maximal = max(len(name) for name in set(name_list))
     if len(name) == maximal:
             print(name)
 
-# c
+# в
 average = sum(age_list) / len(age_list)
 print(average)
 
@@ -54,25 +55,23 @@ my_dict_2 = {"q": 11, "j": "temp", 13: "cat", 15: 5, "b": 100, "u": "three"}
 # a
 common_dict_set = set(my_dict_1.keys()).intersection(set(my_dict_2.keys()))
 print(list(common_dict_set))
-# b
+# б
 unique_key_1 = []
 for key in list(my_dict_1.keys()):
     if key not in common_dict_set:
         unique_key_1.append(key)
 print(unique_key_1)
-# c
+# в
 key_value_1 = {}
 for key, value in my_dict_1.items():
     if key in unique_key_1:
         key_value_1.update({key: value})
 print(key_value_1)
 
-# d
-updated_dict = {}
-updated_dict.update(my_dict_1)
-updated_dict.update(my_dict_2)
+# г
+updated_dict = {**my_dict_1, **my_dict_2}
 for key, value in my_dict_1.items():
-     for key1, value1 in my_dict_2.items():
-            if key == key1:
+     for value1 in my_dict_2.values():
+            if key in common_dict_set:
                 updated_dict[key] = [value, value1]
 print(updated_dict)
