9bf51da7f704c782d7cdae8f4439839bd7a0ff15
topoliant <Awesomeidea19>
2021-02-18 09:24:12 +0200

    Homework 9

--- HW 9.py
+++ HW 9.py
@@ -14,8 +14,7 @@ for word in name_list:
 ####################
 # 2
 
-def create_json_dict():
-    number_of_keys = randint(5, 20)
+def create_json_dict(number_of_keys):
     unique_dict = {}
     index = 0
     while index < number_of_keys:
@@ -28,12 +27,12 @@ def create_json_dict():
         probability = (1/3, 1/3, 1/3)  # если не ошибаюсь, то пайтон по умолчанию выбирает с одинаковой вероятностью?
         inside_value = choices(value_list, probability)
         unique_dict[random_key] = inside_value
-        unique_dict.update({random_key: inside_value[0]}) # иначе value печатается в виде списка
+        unique_dict.update({random_key: inside_value[0]})  # иначе value печатается в виде списка
         index += 1
     return unique_dict
 
 
-print(create_json_dict())
+print(create_json_dict(randint(5, 20)))
 
 #######################
 # 3
@@ -41,8 +40,5 @@ print(create_json_dict())
 
 def write_in_json(file_path):
     with open(file_path, "w") as json_file:
-        json_data = create_json_dict()
+        json_data = create_json_dict(randint(5, 20))
         json.dump(json_data, json_file)
-
-
-print(write_in_json("random_key_json.json"))
